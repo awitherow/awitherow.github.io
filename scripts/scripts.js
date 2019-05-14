9d8f0134b668e613940673784f27dcba2360cb3b
awitherow <au.witherow@gmail.com>
2019-05-13 14:30:33 -1000

    add enhanced navigation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -153,3 +153,104 @@ function removeEmpties(obj) {
   });
   return newObj;
 }
+
+(function printPaths() {
+  var paths = [
+    {
+      id: "fundamentals",
+      title: "1:1 Introduction to Yoga Fundamentals",
+      description:
+        "Learn the fundamentals of Yoga as a Beginner so that you can go to classes and practice with Confidence!",
+      features: [
+        "What Yoga Really Is About",
+        "Components of Class or Daily Practice",
+        "Benefits of Daily Yoga Practice",
+        "The Power of Breath",
+        "Connecting With your Body",
+        "Stilling the Mind",
+      ],
+      link: {
+        href: "https://forms.gle/msGWwpS3yBTHMNf49",
+        text: "Let's Start Yoga",
+      },
+    },
+    {
+      id: "enhance",
+      title: "1:1 Yoga Practitioner Enhancements",
+      description:
+        "A Private Soothing Restorative Yoga Flow with the Healing Power of Reiki and Hands on Realignment Therapy",
+      features: [
+        "Online or In Person",
+        "Empowering Hatha Yoga with Raja Yoga",
+        "Fundamentals of Flow",
+        "Skillwork, Alignment and Balance",
+        "Deepen Your Experience with Breath",
+        "Advanced Kriyas for Ecstatic Conductivity",
+        "Unlocking Bliss Consciousness with Deep Meditation",
+      ],
+      link: {
+        href: "https://forms.gle/apJxLZKFZmjxeMaSA",
+        text: "Start Your Journey",
+      },
+    },
+    {
+      id: "therapy",
+      title: "1:1 Therapeutic Restorative Yoga w/ Reiki",
+      description:
+        "A Private Soothing Restorative Yoga Flow with the Healing Power of Reiki and Hands on Realignment Therapy",
+      features: [
+        "Discover What Your Body Needs",
+        "Deep Relaxing Breathwork",
+        "Personalised Sensually Healing Restorative Yoga Flow",
+        "Gentle Hands on Reiki, and Therapeutic Realignment Adjustments",
+        "Guided Meditation for Deep Relaxation and Healing",
+      ],
+      link: {
+        href: "https://forms.gle/msGWwpS3yBTHMNf49",
+        text: "Start Healing",
+      },
+    },
+    {
+      id: "online",
+      title: "'Movement in Stillness' Self Paced Course",
+      description:
+        "Integrate ancient and modern yogic theory and practice into your lifestyle to find Stillness in the Movements of Life, and the Movement in within your Stillness.",
+      features: [
+        "Learn Yoga beyond Āsana",
+        "Connect Deeply with Your Life Experience",
+        "Discover the Power of Breath",
+        "Find Deep Silence with Meditation",
+        "Discover Alignment with Your Truth",
+      ],
+      link: {
+        href: "https://forms.gle/7dyNLiZc1UCdEhZA8",
+        text: "Course Interest Form",
+      },
+    },
+  ];
+
+  var container = document.getElementById("paths-container");
+  var html = "";
+
+  paths.map(function(path) {
+    html +=
+      "<div id='" +
+      path.id +
+      "'class='path'><h1>" +
+      path.title +
+      "</h1><h2>" +
+      path.description +
+      "</h2><ul>";
+    path.features.map(function(feat) {
+      html += "<li>" + feat + "</li>";
+    });
+    html +=
+      "</ul><p class='go'><a href='" +
+      path.link.href +
+      "' target='_blank' class='button'>" +
+      path.link.text +
+      " <i class='far fa-arrow-alt-circle-right'></i></a></p></div>";
+  });
+
+  container.innerHTML += html;
+})();
